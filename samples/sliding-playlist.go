34dfb4a934bc64e4eaefd57134600f75fee42018
Alexander I.Grafov (Axel) <grafov@gmail.com>
2013-02-21 01:11:06 +0400

    samples

---
+++ samples/sliding-playlist.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/grafov/m3u8"
+)
+
+func main() {
+	s := m3u8.NewSlidingPlaylist(4)
+	for i:=0; i<10; i++ {
+		s.AddSegment(m3u8.Segment{0, "sample.ts", 5.0, nil})
+		if i % 5 == 1 {
+			print(s.Buffer().String());	print("\n")
+		}
+	}
+	print(s.Buffer().String());	print("\n")
+	print(s.Buffer().String());	print("\n")
+}
