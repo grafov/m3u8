368062e2299e860b62e9659e642481df869a180f
Alexander I.Grafov (Axel) <grafov@gmail.com>
2013-07-03 23:50:08 +0400

    draft

--- reader.go
+++ reader.go
@@ -10,9 +10,56 @@ package m3u8
 */
 
 import (
-	"bytes"
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
 )
 
-func Load(buf bytes.Buffer) *Playlist {
+func (p *FixedPlaylist) Load(reader io.Reader) error {
+	var eof, started, streamInf bool
+	var seqid uint64
+
+	buf := bufio.NewReader(reader)
+
+	for !eof {
+		line, err := buf.ReadString('\n')
+		if err == io.EOF {
+			eof = true
+		} else if err != nil {
+			break
+		}
+		// start tag first
+		if line == "#EXTM3U" {
+			started = true
+		}
+		// version tag
+		if started && strings.HasPrefix(line, "#EXT-X-VERSION:") {
+			_, err = fmt.Sscanf(line, "#EXT-X-VERSION:%d", p.ver)
+			return err
+		}
+		if started && strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
+			streamInf = true
+			_, err = fmt.Sscanf(line, "", p.ver)
+
+			seqid++
+			//seg := Segment{SeqId: seqid
+		}
+		if started && streamInf {
+
+		}
+	}
 	return nil
+}
+
+func (p *SlidingPlaylist) Load(reader io.Reader) {
+	//	buf := bufio.NewReader(reader)
+
+	//line, err := buf.ReadString('\n')
+}
+
+func (p *VariantPlaylist) Load(reader io.Reader) {
+	// buf := bufio.NewReader(reader)
+
+	//	line, err := buf.ReadString('\n')
 }
