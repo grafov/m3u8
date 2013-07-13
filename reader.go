4be367fdb40d6c143fb13816544b65901568a90c
Alexander I.Grafov (Axel) <grafov@gmail.com>
2013-07-13 23:49:06 +0400

    Parsing master playlists. Mostly works.

--- reader.go
+++ reader.go
@@ -11,15 +11,17 @@ package m3u8
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 )
 
 // Read and parse master playlist.
-func (p *MasterPlaylist) Decode(reader io.Reader) error {
-	var eof, started, streamInf bool
-	var seqid uint64
+// Call with strict=true will stop parsing on first format error.
+func (p *MasterPlaylist) Decode(reader io.Reader, strict bool) error {
+	var eof, m3u, tagInf bool
+	var variant *Variant
 
 	buf := bufio.NewReader(reader)
 
@@ -30,31 +32,81 @@ func (p *MasterPlaylist) Decode(reader io.Reader) error {
 		} else if err != nil {
 			break
 		}
+		line = strings.TrimSpace(line)
 		// start tag first
-		if line == "#EXTM3U" {
-			started = true
+		if strings.HasPrefix(line, "#EXTM3U") {
+			m3u = true
 		}
 		// version tag
-		if started && strings.HasPrefix(line, "#EXT-X-VERSION:") {
-			_, err = fmt.Sscanf(line, "#EXT-X-VERSION:%d", p.ver)
-			return err
+		if strings.HasPrefix(line, "#EXT-X-VERSION:") {
+			_, err = fmt.Sscanf(line, "#EXT-X-VERSION:%d", &p.ver)
+			if strict && err != nil {
+				return err
+			}
 		}
-		if started && strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
-			streamInf = true
-			_, err = fmt.Sscanf(line, "", p.ver)
-
-			seqid++
-			//seg := Segment{SeqId: seqid
+		if !tagInf && strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
+			tagInf = true
+			variant = new(Variant)
+			p.variants = append(p.variants, variant)
+			for _, param := range strings.Split(line[18:], ",") {
+				if strings.HasPrefix(param, "PROGRAM-ID") {
+					_, err = fmt.Sscanf(param, "PROGRAM-ID=%d", &variant.ProgramId)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "BANDWIDTH") {
+					_, err = fmt.Sscanf(param, "BANDWIDTH=%d", &variant.Bandwidth)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "CODECS") {
+					_, err = fmt.Sscanf(param, "CODECS=%s", &variant.Codecs)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "RESOLUTION") {
+					_, err = fmt.Sscanf(param, "RESOLUTION=%s", &variant.Resolution)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "AUDIO") {
+					_, err = fmt.Sscanf(param, "AUDIO=%s", &variant.Audio)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "VIDEO") {
+					_, err = fmt.Sscanf(param, "VIDEO=%s", &variant.Video)
+					if strict && err != nil {
+						return err
+					}
+				}
+				if strings.HasPrefix(param, "SUBTITLES") {
+					_, err = fmt.Sscanf(param, "SUBTITLES=%s", &variant.Subtitles)
+					if strict && err != nil {
+						return err
+					}
+				}
+			}
+			continue
 		}
-		if started && streamInf {
-
+		if tagInf {
+			tagInf = false
+			variant.URI = line
 		}
+	}
+	if strict && !m3u {
+		return errors.New("#EXT3MU absent")
 	}
 	return nil
 }
 
 func (p *MediaPlaylist) Decode(reader io.Reader) error {
-	var eof, started, streamInf bool
+	var eof, started, tagInf bool
 	var seqid uint64
 
 	buf := bufio.NewReader(reader)
@@ -76,13 +128,12 @@ func (p *MediaPlaylist) Decode(reader io.Reader) error {
 			return err
 		}
 		if started && strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
-			streamInf = true
+			tagInf = true
 			_, err = fmt.Sscanf(line, "", p.ver)
 
 			seqid++
-			//seg := Segment{SeqId: seqid
 		}
-		if started && streamInf {
+		if started && tagInf {
 
 		}
 	}
