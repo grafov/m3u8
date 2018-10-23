ce739208d560cc96a90baac77dd4bff4a2e64ebe
Omar Qazi <omar@smick.co>
2018-10-22 22:42:43 -0700

    add an extra newline after the independent segments thing

--- writer.go
+++ writer.go
@@ -78,7 +78,7 @@ func (p *MasterPlaylist) Encode() *bytes.Buffer {
 	p.buf.WriteString("#EXTM3U\n#EXT-X-VERSION:")
 	p.buf.WriteString(strver(p.ver))
 	p.buf.WriteRune('\n')
-	p.buf.WriteString("#EXT-X-INDEPENDENT-SEGMENTS\n")
+	p.buf.WriteString("#EXT-X-INDEPENDENT-SEGMENTS\n\n")
 
 	var altsWritten map[string]bool = make(map[string]bool)
 
