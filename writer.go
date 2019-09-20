dcf7b4a0077ba76fea73de3ddd80b26efae86705
Roman Kollár <10644651+rkollar@users.noreply.github.com>
2019-09-20 14:06:21 +0200

    fix FORCED attribute improper quotation

--- writer.go
+++ writer.go
@@ -136,9 +136,8 @@ func (p *MasterPlaylist) Encode() *bytes.Buffer {
 					p.buf.WriteRune('"')
 				}
 				if alt.Forced != "" {
-					p.buf.WriteString(",FORCED=\"")
+					p.buf.WriteString(",FORCED=")
 					p.buf.WriteString(alt.Forced)
-					p.buf.WriteRune('"')
 				}
 				if alt.Characteristics != "" {
 					p.buf.WriteString(",CHARACTERISTICS=\"")
